9308baeb129c106693bef498c6b2d374fb432208
ElanPodo <elanpodo@gmail.com>
2026-04-25 02:13:18 -0700

    Fix JSON encoding issue and use pathlib to anchor paths to project root

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,8 @@
 import pandas as pd
-oc = pd.read_csv("C:/Users/elanp/Documents/projects/volatility-surface-signals/data/spy_option_chain.csv", parse_dates=["date", "expiration"], encoding='utf-16')
-oc.to_parquet("C:/Users/elanp/Documents/projects/volatility-surface-signals/data/spy_option_chain.parquet")
-vh = pd.read_csv("C:/Users/elanp/Documents/projects/volatility-surface-signals/data/spy_volatility_history.csv", parse_dates=["date"], encoding='utf-16')
-vh.to_parquet("C:/Users/elanp/Documents/projects/volatility-surface-signals/data/spy_volatility_history.parquet")
+
+oc = pd.read_csv("data/spy_option_chain.csv", parse_dates=["date", "expiration"])
+oc.to_parquet("data/spy_option_chain.parquet")
+
+vh = pd.read_csv("data/spy_volatility_history.csv", parse_dates=["date"])
+vh.to_parquet("data/spy_volatility_history.parquet")
     
