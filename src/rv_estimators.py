b83ac58cd5c009aea85a7b418537b7b4bb97504f
ElanPodo <elanpodo@gmail.com>
2026-04-26 14:28:24 -0700

    Compute Parkinson RV estimator

--- src/rv_estimators.py
+++ src/rv_estimators.py
@@ -9,3 +9,11 @@ def close_to_close_rv(prices, window=21, annualize=True, trading_days=252):
         rv = rv * np.sqrt(trading_days)
     return rv
 
+def parkinson_rv(high_price, low_price, window=21, annualize=True, trading_days=252):
+    log_diff_square = np.square(np.log(high_price / low_price))
+    constant = 1 / (4 * window * np.log(2))
+    rv = np.sqrt(constant * log_diff_square.rolling(window=window).sum())
+    if annualize:
+        rv = rv * np.sqrt(trading_days)
+    return rv
+
